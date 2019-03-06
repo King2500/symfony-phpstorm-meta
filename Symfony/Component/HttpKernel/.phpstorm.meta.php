c49fa317c28c786335aed522554f8a516aba15ca
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2019-03-06 18:11:20 +0100

    Added meta for HttpKernel

---
+++ Symfony/Component/HttpKernel/.phpstorm.meta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PHPSTORM_META {
+	registerArgumentsSet('symfony_http_kernel_requests',
+		\Symfony\Component\HttpKernel\HttpKernelInterface::MASTER_REQUEST,
+		\Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST
+	);
+
+	expectedArguments(\Symfony\Component\HttpKernel\HttpKernelInterface::handle(), 1, argumentsSet('symfony_http_kernel_requests'));
+	expectedArguments(\Symfony\Component\HttpKernel\HttpKernel::handle(), 1, argumentsSet('symfony_http_kernel_requests'));
+	expectedArguments(\Symfony\Component\HttpKernel\HttpCache\HttpCache::handle(), 1, argumentsSet('symfony_http_kernel_requests'));
+	expectedArguments(\Symfony\Component\HttpKernel\Kernel::handle(), 1, argumentsSet('symfony_http_kernel_requests'));
+	expectedArguments(\Symfony\Component\HttpKernel\Event\KernelEvent::__construct(), 2, argumentsSet('symfony_http_kernel_requests'));
+	expectedArguments(\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent::__construct(), 2, argumentsSet('symfony_http_kernel_requests'));
+
+	expectedReturnValues(\Symfony\Component\HttpKernel\Event\KernelEvent::getRequestType(), argumentsSet('symfony_http_kernel_requests'));
+
+	expectedArguments(\Symfony\Component\HttpKernel\Fragment\FragmentHandler::render(), 1, 'inline', 'esi', 'ssi', 'hinclude');
+}
