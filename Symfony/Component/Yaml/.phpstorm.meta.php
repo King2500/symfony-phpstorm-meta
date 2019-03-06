b86ab4ed8c548ecc1ba20fa0bf78efc821765b20
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2019-03-06 18:20:49 +0100

    Added meta for Yaml component

---
+++ Symfony/Component/Yaml/.phpstorm.meta.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PHPSTORM_META {
+
+	registerArgumentsSet('symfony_yaml_parse_flags',
+		\Symfony\Component\Yaml\Yaml::PARSE_EXCEPTION_ON_INVALID_TYPE |
+		\Symfony\Component\Yaml\Yaml::PARSE_OBJECT |
+		\Symfony\Component\Yaml\Yaml::PARSE_OBJECT_FOR_MAP |
+		\Symfony\Component\Yaml\Yaml::PARSE_DATETIME |
+		\Symfony\Component\Yaml\Yaml::PARSE_CONSTANT |
+		\Symfony\Component\Yaml\Yaml::PARSE_CUSTOM_TAGS
+	);
+	expectedArguments(\Symfony\Component\Yaml\Yaml::parseFile(), 1, argumentsSet('symfony_yaml_parse_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Yaml::parse(), 1, argumentsSet('symfony_yaml_parse_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Parser::parseFile(), 1, argumentsSet('symfony_yaml_parse_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Parser::parse(), 1, argumentsSet('symfony_yaml_parse_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Inline::parse(), 1, argumentsSet('symfony_yaml_parse_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Inline::initialize(), 0, argumentsSet('symfony_yaml_parse_flags'));
+
+	registerArgumentsSet('symfony_yaml_dump_flags',
+		\Symfony\Component\Yaml\Yaml::DUMP_OBJECT |
+		\Symfony\Component\Yaml\Yaml::DUMP_EXCEPTION_ON_INVALID_TYPE |
+		\Symfony\Component\Yaml\Yaml::DUMP_OBJECT_AS_MAP |
+		\Symfony\Component\Yaml\Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK |
+		\Symfony\Component\Yaml\Yaml::DUMP_EMPTY_ARRAY_AS_SEQUENCE
+	);
+	expectedArguments(\Symfony\Component\Yaml\Yaml::dump(), 3, argumentsSet('symfony_yaml_dump_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Dumper::dump(), 3, argumentsSet('symfony_yaml_dump_flags'));
+	expectedArguments(\Symfony\Component\Yaml\Inline::dump(), 1, argumentsSet('symfony_yaml_dump_flags'));
+}
