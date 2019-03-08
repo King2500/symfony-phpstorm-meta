13bc090564fdc75e61270312249691310df19cc1
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2019-03-08 18:35:41 +0100

    Added meta for DependencyInjection component

---
+++ Symfony/Component/DependencyInjection/.phpstorm.meta.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PHPSTORM_META {
+
+	registerArgumentsSet('symfony_di_compiler_pass_types',
+		\Symfony\Component\DependencyInjection\Compiler\PassConfig::TYPE_BEFORE_OPTIMIZATION,
+		\Symfony\Component\DependencyInjection\Compiler\PassConfig::TYPE_BEFORE_REMOVING,
+		\Symfony\Component\DependencyInjection\Compiler\PassConfig::TYPE_AFTER_REMOVING,
+		\Symfony\Component\DependencyInjection\Compiler\PassConfig::TYPE_OPTIMIZE,
+		\Symfony\Component\DependencyInjection\Compiler\PassConfig::TYPE_REMOVE
+	);
+	expectedArguments(\Symfony\Component\DependencyInjection\Compiler\Compiler::addPass(), 1, argumentsSet('symfony_di_compiler_pass_types'));
+	expectedArguments(\Symfony\Component\DependencyInjection\Compiler\PassConfig::addPass(), 1, argumentsSet('symfony_di_compiler_pass_types'));
+	expectedArguments(\Symfony\Component\DependencyInjection\Compiler\MergeExtensionConfigurationContainerBuilder::addCompilerPass(), 1, argumentsSet('symfony_di_compiler_pass_types'));
+	expectedArguments(\Symfony\Component\DependencyInjection\ContainerBuilder::addCompilerPass(), 1, argumentsSet('symfony_di_compiler_pass_types'));
+
+	registerArgumentsSet('symfony_di_invalid_ref_behaviors',
+		\Symfony\Component\DependencyInjection\ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE,
+		\Symfony\Component\DependencyInjection\ContainerInterface::RUNTIME_EXCEPTION_ON_INVALID_REFERENCE,
+		\Symfony\Component\DependencyInjection\ContainerInterface::NULL_ON_INVALID_REFERENCE,
+		\Symfony\Component\DependencyInjection\ContainerInterface::IGNORE_ON_INVALID_REFERENCE,
+		\Symfony\Component\DependencyInjection\ContainerInterface::IGNORE_ON_UNINITIALIZED_REFERENCE
+	);
+	expectedArguments(\Symfony\Component\DependencyInjection\ContainerInterface::get(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\Container::get(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\Container::make(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\ContainerBuilder::get(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\ContainerBuilder::doGet(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\Reference::__construct(), 1, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedArguments(\Symfony\Component\DependencyInjection\TypedReference::__construct(), 2, argumentsSet('symfony_di_invalid_ref_behaviors'));
+	expectedReturnValues(\Symfony\Component\DependencyInjection\Reference::getInvalidBehavior(), argumentsSet('symfony_di_invalid_ref_behaviors'));
+}
