0b9528b29b76c1a24e3066f48133814b8c379e38
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2019-03-28 19:25:10 +0100

    Added meta for Config component

---
+++ Symfony/Component/Config/.phpstorm.meta.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    expectedArguments(\Symfony\Component\Config\Resource\FileResource::__construct(), 0, __FILE__);
+    expectedArguments(\Symfony\Component\Config\Resource\DirectoryResource::__construct(), 0, __DIR__);
+    expectedArguments(\Symfony\Component\Config\Resource\DirectoryResource::__construct(), 1, '/\.php$/', '/\.xml$/', '/\.yaml$/');
+
+}
