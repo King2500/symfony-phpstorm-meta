b7ffe84e1d8872fd08229ee76ccd0d872a039bde
Thomas Schulz <mail@king2500.net>
2025-08-23 01:50:16 +0200

    Added meta for IpUtils::anonymize v4Bytes/v6Bytes #25 (fixes #25)

--- Symfony/Component/HttpFoundation/.phpstorm.meta.php
+++ Symfony/Component/HttpFoundation/.phpstorm.meta.php
@@ -383,4 +383,7 @@ namespace PHPSTORM_META {
         FILTER_FLAG_PATH_REQUIRED |
         FILTER_FLAG_QUERY_REQUIRED
     );
+
+    expectedArguments(\Symfony\Component\HttpFoundation\IpUtils::anonymize(), 1, 0, 1, 2, 3, 4);
+    expectedArguments(\Symfony\Component\HttpFoundation\IpUtils::anonymize(), 2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
 }
