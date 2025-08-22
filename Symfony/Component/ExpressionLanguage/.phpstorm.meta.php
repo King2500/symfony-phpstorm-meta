bf7e3233017042c1881f7fbf53876fdb00c4bda4
Thomas Schulz <mail@king2500.net>
2025-08-23 01:11:11 +0200

    Added meta for ExpressionLanguage Linter/Parser flags added in Sf 7.1 (fixes #21)

--- Symfony/Component/ExpressionLanguage/.phpstorm.meta.php
+++ Symfony/Component/ExpressionLanguage/.phpstorm.meta.php
@@ -12,4 +12,12 @@ namespace PHPSTORM_META {
 	);
 	expectedArguments(\Symfony\Component\ExpressionLanguage\Token::__construct(), 0, argumentsSet('symfony_expression_token_types'));
 	expectedArguments(\Symfony\Component\ExpressionLanguage\Token::test(), 0, argumentsSet('symfony_expression_token_types'));
+
+	// Symfony 7.1: Parser::lint()/parse() accept flags to ignore unknown variables/functions
+	registerArgumentsSet('symfony_expression_parser_flags',
+		\Symfony\Component\ExpressionLanguage\Parser::IGNORE_UNKNOWN_VARIABLES |
+		\Symfony\Component\ExpressionLanguage\Parser::IGNORE_UNKNOWN_FUNCTIONS
+	);
+	expectedArguments(\Symfony\Component\ExpressionLanguage\Parser::lint(), 2, argumentsSet('symfony_expression_parser_flags'));
+	expectedArguments(\Symfony\Component\ExpressionLanguage\Parser::parse(), 2, argumentsSet('symfony_expression_parser_flags'));
 }
